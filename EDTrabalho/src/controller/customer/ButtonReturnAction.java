e487f8b4ce84d277b5dbb2309361953746fd490f
maranefabio <84942346+maranefabio@users.noreply.github.com>
2024-06-04 21:41:29 -0300

    Button listeners e actions adicionados

---
+++ EDTrabalho/src/controller/customer/ButtonReturnAction.java
@@ -0,0 +1,12 @@
+package controller.customer;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonReturnAction implements ActionListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Return button clicked");
+    }
+}
